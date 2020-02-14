e4260c8979e217869708206311004f6462d2721a
Murilo Baldi <murilobaldi@tutanota.com>
2020-02-14 16:15:56 -0300

    Rename files and add background scene

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
 
+var server_port = 5000
+var server_address = '127.0.0.1'
+
 # animation state machine
 var state_machine
 var speed = 200
@@ -10,7 +13,9 @@ var attack_cooldown_time = 500
 var next_attack_time = 0
 var attack_damage = 10
 
-func check_flip_sprite():
+# var minion = load('res://scenes/Player.tscn')
+
+func check_and_flip_sprite():
 	if velocity.x < 0:
 		$Sprite.flip_h = true
 	else:
@@ -21,11 +26,10 @@ func do_attack(target):
 	var now = OS.get_ticks_msec()
 	if now >= next_attack_time:
 		state_machine.travel('attack')
-		target.take_damage(self.attack_damage)
+		# target.take_damage(self.attack_damage)
 		# Add cooldown time to current time
 		next_attack_time = now + attack_cooldown_time
 
-# warning-ignore:unused_argument
 func _input(event):
 	var mouse_pos = get_global_mouse_position()
 	
@@ -36,7 +40,7 @@ func _input(event):
 
 	velocity = velocity.normalized() * speed
 	
-	check_flip_sprite()
+	check_and_flip_sprite()
 	
 	if velocity == Vector2.ZERO:
 		state_machine.travel('idle')
@@ -46,9 +50,12 @@ func _input(event):
 func _ready():
 	state_machine = $AnimationTree.get('parameters/playback')
 	pass
-	
+
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
+
+	get_tree().get_root().get_node('Network').signal_movement\
+	(get_tree().get_network_unique_id(), self.get_position())
 	if collision:
 		var collider_name = collision.collider.name.to_lower()
 		if not 'player' in collider_name:
